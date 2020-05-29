1fd77f8f5f597f1f6536c45004ed456745fd10df
minemidnight <minemidnightmc@gmail.com>
2020-05-28 22:47:07 -0400

    Change to submodule for bucket protobuf

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const bucketClient = new BucketClient();
 const eventSocket = new EventSocket();
 
 async function init() {
-	const rpcProto = await protobuf.load(path.resolve(__dirname, "..", "protobuf", "rpcWrapper.proto"));
-	const discordProto = await protobuf.load(path.resolve(__dirname, "..", "protobuf", "discordapi", "service.proto"));
+	const rpcProto = await protobuf.load(path.resolve(__dirname, "..", "bucket-proto", "rpcWrapper.proto"));
+	const discordProto = await protobuf.load(path.resolve(__dirname, "..", "bucket-proto", "service.proto"));
 	bucketClient.start({
 		discord: discordProto,
 		rpc: rpcProto
