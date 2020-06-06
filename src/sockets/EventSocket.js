1229c0b008ab85422c14825446c2e932e7675a37
minemidnight <minemidnightmc@gmail.com>
2020-06-06 01:18:13 -0400

    Update zeromq to v6

--- src/sockets/EventSocket.js
+++ src/sockets/EventSocket.js
@@ -1,11 +1,11 @@
 const EventEmitter = require("events");
-const zmq = require("zeromq");
+const { Pull } = require("zeromq");
 
 class EventSocket extends EventEmitter {
 	constructor() {
 		super();
-		this.socket = zmq.socket("pull");
-		this.socket.on("message", this.message.bind(this));
+		this.socket = new Pull();
+		this.messageHandler();
 
 		this.proto = null;
 	}
@@ -15,9 +15,10 @@ class EventSocket extends EventEmitter {
 		this.socket.connect(`tcp://event-handler-zmq-proxy:${process.env.EVENT_HANDLER_ZMQ_PROXY_SERVICE_PORT_PUSH}`);
 	}
 
-	message(message) {
-		// TODO
-		// this.emit("message", this.proto.lookup("Command").decode(message));
+	async messageHandler() {
+		while(!this.socket.closed) {
+			const [message] = await this.socket.receive();
+		}
 	}
 
 	close() {
